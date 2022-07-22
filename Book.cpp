8034fcb2e8e2d2235d0f5b6c2430ea7afbee4f2e
Paul Adams <ghusiyy@gmail.com>
2022-07-21 23:11:25 -0700

    create Book getters

--- Book.cpp
+++ Book.cpp
@@ -1,3 +1,60 @@
 #include "Book.h"
 
-Book::Book(){};
+Book::Book(std::vector<Contributor> authors,
+           std::vector<Contributor> translators,
+           std::vector<Contributor> editors,
+           std::string title,
+           std::string edition,
+           std::string pub_date,
+           std::string publisher,
+           int pagination)
+{
+    this->authors = authors;
+    this->translators = translators;
+    this->editors = editors;
+    this->title = title;
+    this->edition = edition;
+    this->pub_date = pub_date;
+    this->publisher = publisher;
+    this->pagination = pagination;
+};
+
+std::vector<Contributor> Book::get_authors()
+{
+    return this->authors;
+}
+
+std::vector<Contributor> Book::get_translators()
+{
+    return this->translators;
+}
+
+std::vector<Contributor> Book::get_editors()
+{
+    return this->editors;
+}
+
+std::string Book::get_title()
+{
+    return this->title;
+}
+
+std::string Book::get_edition()
+{
+    return this->edition;
+}
+
+std::string Book::get_pub_date()
+{
+    return this->pub_date;
+}
+
+std::string Book::get_publisher()
+{
+    return this->publisher;
+}
+
+int Book::get_pagination()
+{
+    return this->pagination;
+}
--- Book.h
+++ Book.h
@@ -16,7 +16,22 @@ private:
     int pagination;
 
 public:
-    Book();
+    Book(std::vector<Contributor> authors,
+         std::vector<Contributor> translators,
+         std::vector<Contributor> editors,
+         std::string title,
+         std::string edition,
+         std::string pub_date,
+         std::string publisher,
+         int pagination);
+    std::vector<Contributor> get_authors();
+    std::vector<Contributor> get_translators();
+    std::vector<Contributor> get_editors();
+    std::string get_title();
+    std::string get_edition();
+    std::string get_pub_date();
+    std::string get_publisher();
+    int get_pagination();
 };
 
 #endif
