d6e9c5d0e4606a4845a34b9266e125324acbea38
Paul Adams <ghusiyy@gmail.com>
2022-07-24 23:47:12 -0700

    read and parse json

--- BookInventoryCoordinator.cpp
+++ BookInventoryCoordinator.cpp
@@ -1,5 +1,25 @@
 #include "BookInventoryCoordinator.h"
-
+#include <vector>
+#include <fstream>
+#include "nlohmann/json.hpp"
+using namespace nlohmann;
 BookInventoryCoordinator::BookInventoryCoordinator(){};
+
+void BookInventoryCoordinator::seed_inventory()
+{
+    std::ifstream f("books.json");
+
+    json data = json::parse(f);
+};
+
+void BookInventoryCoordinator::set_books(std::vector<Book> *books)
+{
+    this->books = books;
+};
+
+std::vector<Book> *BookInventoryCoordinator::get_books()
+{
+    return this->books;
+};
 
 BookInventoryCoordinator::~BookInventoryCoordinator(){};
--- BookInventoryCoordinator.h
+++ BookInventoryCoordinator.h
@@ -1,11 +1,17 @@
 #ifndef BookInventoryCoordinator_H
 #define BookInventoryCoordinator_H
-
+#include <vector>
+#include "Book.h"
 class BookInventoryCoordinator
 {
 private:
+    std::vector<Book> *books;
+
 public:
     BookInventoryCoordinator();
+    void seed_inventory();
+    void set_books(std::vector<Book> *books);
+    std::vector<Book> *get_books();
     ~BookInventoryCoordinator();
 };
 
