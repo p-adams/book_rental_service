3981b94370eaa03c4498b23cfffe9cd2b22d4109
Paul Adams <ghusiyy@gmail.com>
2022-08-12 22:06:50 -0700

    add role setter

--- Me.cpp
+++ Me.cpp
@@ -17,6 +17,16 @@ std::string Me::get_password()
     return this->password;
 }
 
+std::string Me::get_role()
+{
+    return this->role;
+}
+
+void Me::set_role(std::string role)
+{
+    this->role = role;
+}
+
 Me::~Me()
 {
 }
