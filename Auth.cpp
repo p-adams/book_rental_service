12c84882659730ee714a7f455e7d7dbef7d2d308
Paul Adams <ghusiyy@gmail.com>
2022-08-12 22:55:19 -0700

    add credential namespace

--- Auth.cpp
+++ Auth.cpp
@@ -1,6 +1,10 @@
 #include "Auth.h"
 #include <iostream>
 #include <string>
+#include <fstream>
+#include "nlohmann/json.hpp"
+
+using namespace nlohmann;
 
 enum AuthType
 {
@@ -17,6 +21,16 @@ void Auth::check_status(std::string username, std::string password)
 {
     this->username = username;
     this->password = password;
+    std::ifstream f("../creds.json");
+    json data = json::parse(f);
+    try
+    {
+        auto credentials = data.get<ns::credentials>();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
 
 void Auth::login()
--- Auth.h
+++ Auth.h
@@ -1,8 +1,29 @@
 #ifndef AUTH_H
 #define AUTH_H
 #include <string>
+#include "nlohmann/json.hpp"
+
+using namespace nlohmann;
+
+namespace ns
+{
+    class credentials
+    {
+    private:
+    public:
+        credentials() = default;
+        std::string username;
+        std::string password;
+        credentials(std::string username, std::string password) : username(std::move(username)), password(std::move(password))
+        {
+        }
+        NLOHMANN_DEFINE_TYPE_INTRUSIVE(credentials, username, password)
+    };
+
+}
 
 class Auth
+
 {
 private:
     std::string username;
